b98b6b5b39fa446ee5e20ea95f35cb253e37f8b3
Teddy Cubaka <teddycubaka96@gmail.com>
2022-09-13 11:46:07 +0100

    selector des valeurs des champs dans les input

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ imgUploaded.addEventListener( 'change' , ()=>{
     displayerImg.src = URL.createObjectURL(picture)
   }
 })
+
+let firstName = ""; 
+let secondName = ""; 
+let groupe = ""; 
+let bio = ""; 
+
+function valuesSelector (){
+  firstName = document.querySelector("#prénom").value; 
+  secondName = document.querySelector("#nom").value; 
+  groupe = document.querySelector("#groupe").value; 
+  bio = document.querySelector("#zoneDuBio").value;
+}
+
+document.querySelector("#blueBtn").addEventListener('click',() =>{
+  valuesSelector();
+})
+
+// let userFirstName = "";
+// let userSecondName = "";
+// let userGroupe = ""
+// function seter(){
+
+// }
